dca57c8d9e32b4f5422c606780898f04a51f717a
Saliha174 <saliha.bekhti@yahoo.fr>
2025-09-24 16:56:13 +0200

    Ajout 24/09

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btnOpen = document.querySelector(".logoAccueil");
-  const btnClose = document.querySelector(".logoMenu");
+  const btnClose = document.querySelector(".logoCroix");
   const accueil = document.getElementById("accueil");
   const menu = document.getElementById("menu");
 
   btnOpen.addEventListener("click", () => {
     menu.classList.add("active");
     accueil.classList.add("littleAccueil");
+    //   setTimeout(() => {
+    //     window.location.href = link.href;
+    //   }, 500);
   });
 
   btnClose.addEventListener("click", () => {
